141ef78c0478c59dbef778798a999f5ee0ed386a
Joon Ki Chun <joon0858@naver.com>
2023-04-27 13:19:42 +0900

    20230427

---
+++ baekjoon/4. 1차원 배열/beakjoon_python_10813_공바꾸기.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+answer = [i for i in range(1, n+1)]
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    answer[i-1], answer[j-1] = answer[j-1], answer[i-1]
+
+print(' '.join(str(n) for n in answer))
